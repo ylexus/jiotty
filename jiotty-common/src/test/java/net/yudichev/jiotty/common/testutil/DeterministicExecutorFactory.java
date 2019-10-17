f318e19c0f1e03ad8ca702eeef4bd824d7850671
ylexus <11aPaLEX>
2019-10-17 21:47:44 +0100

    move inner class to outer for convenience

--- jiotty-common/src/test/java/net/yudichev/jiotty/common/testutil/DeterministicExecutorFactory.java
+++ jiotty-common/src/test/java/net/yudichev/jiotty/common/testutil/DeterministicExecutorFactory.java
@@ -2,10 +2,9 @@ package net.yudichev.jiotty.common.testutil;
 
 import net.yudichev.jiotty.common.async.ExecutorFactory;
 import net.yudichev.jiotty.common.async.SchedulingExecutor;
-import net.yudichev.jiotty.common.lang.Closeable;
 
-import java.time.Duration;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import static java.util.Collections.unmodifiableList;
 
@@ -21,52 +20,5 @@ public final class DeterministicExecutorFactory implements ExecutorFactory {
 
     public List<DeterministicExecutor> getCreatedExecutors() {
         return unmodifiableList(createdExecutors);
-    }
-
-    public static class DeterministicExecutor implements SchedulingExecutor {
-        private Collection<Runnable> commands = new ArrayList<>();
-        private Map<Duration, List<Runnable>> scheduledTasksByDuration = new TreeMap<>();
-
-        @Override
-        public void execute(Runnable command) {
-            commands.add(command);
-        }
-
-        @Override
-        public Closeable schedule(Duration delay, Runnable command) {
-            scheduledTasksByDuration.computeIfAbsent(delay, duration -> new ArrayList<>()).add(command);
-            return Closeable.idempotent(() -> scheduledTasksByDuration.compute(delay, (duration, tasks) -> {
-                if (tasks != null) {
-                    tasks.remove(command);
-                    if (tasks.isEmpty()) {
-                        return null;
-                    }
-                }
-                return tasks;
-            }));
-        }
-
-        @Override
-        public Closeable scheduleAtFixedRate(Duration initialDelay, Duration period, Runnable command) {
-            throw new UnsupportedOperationException("scheduleAtFixedRate");
-        }
-
-        @Override
-        public void close() {
-        }
-
-        public void executePendingCommands() {
-            Collection<Runnable> currentCommands = commands;
-            commands = new ArrayList<>();
-            currentCommands.forEach(Runnable::run);
-        }
-
-        public void executePendingScheduledTasks() {
-            Collection<List<Runnable>> currentCommands = scheduledTasksByDuration.values();
-            scheduledTasksByDuration = new TreeMap<>();
-            currentCommands.stream()
-                    .flatMap(Collection::stream)
-                    .forEach(Runnable::run);
-        }
     }
 }
