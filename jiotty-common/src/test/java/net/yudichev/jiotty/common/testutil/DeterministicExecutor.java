a99f1c425c4d8e00cefc27bc83a2014ce4b8d9a8
ylexus <11aPaLEX>
2019-10-22 08:20:37 +0100

    DeterministicExecutor to support periodic tasks

--- jiotty-common/src/test/java/net/yudichev/jiotty/common/testutil/DeterministicExecutor.java
+++ jiotty-common/src/test/java/net/yudichev/jiotty/common/testutil/DeterministicExecutor.java
@@ -2,6 +2,9 @@ package net.yudichev.jiotty.common.testutil;
 
 import net.yudichev.jiotty.common.async.SchedulingExecutor;
 import net.yudichev.jiotty.common.lang.Closeable;
+import net.yudichev.jiotty.common.lang.PackagePrivateImmutablesStyle;
+import org.immutables.value.Value;
+import org.immutables.value.Value.Immutable;
 
 import java.time.Duration;
 import java.util.*;
@@ -9,6 +12,7 @@ import java.util.*;
 public final class DeterministicExecutor implements SchedulingExecutor {
     private Collection<Runnable> commands = new ArrayList<>();
     private Map<Duration, List<Runnable>> scheduledTasksByDuration = new TreeMap<>();
+    private final Map<Duration, List<ScheduledPeriodicTask>> scheduledPeriodicTasksByInitialDelay = new TreeMap<>();
 
     @Override
     public void execute(Runnable command) {
@@ -31,11 +35,29 @@ public final class DeterministicExecutor implements SchedulingExecutor {
 
     @Override
     public Closeable scheduleAtFixedRate(Duration initialDelay, Duration period, Runnable command) {
-        throw new UnsupportedOperationException("scheduleAtFixedRate");
+        ScheduledPeriodicTask scheduledPeriodicTask = ScheduledPeriodicTask.of(period, command);
+        scheduledPeriodicTasksByInitialDelay
+                .computeIfAbsent(initialDelay, duration -> new ArrayList<>())
+                .add(scheduledPeriodicTask);
+        return Closeable.idempotent(() -> scheduledPeriodicTasksByInitialDelay.compute(initialDelay, (duration, tasks) -> {
+            if (tasks != null) {
+                tasks.remove(scheduledPeriodicTask);
+                if (tasks.isEmpty()) {
+                    return null;
+                }
+            }
+            return tasks;
+        }));
     }
 
     @Override
     public void close() {
+    }
+
+    public void executePendingCommandsUntilIdle() {
+        while (!commands.isEmpty()) {
+            executePendingCommands();
+        }
     }
 
     public void executePendingCommands() {
@@ -50,5 +72,21 @@ public final class DeterministicExecutor implements SchedulingExecutor {
         currentCommands.stream()
                 .flatMap(Collection::stream)
                 .forEach(Runnable::run);
+    }
+
+    public void executePeriodicTasks() {
+        scheduledPeriodicTasksByInitialDelay.values().stream()
+                .flatMap(Collection::stream)
+                .forEach(scheduledPeriodicTask -> scheduledPeriodicTask.task().run());
+    }
+
+    @Immutable
+    @PackagePrivateImmutablesStyle
+    interface BaseScheduledPeriodicTask {
+        @Value.Parameter
+        Duration period();
+
+        @Value.Parameter
+        Runnable task();
     }
 }
