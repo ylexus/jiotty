6ae358cf8727d03f93097ee4abc00260c270644f
ylexus <11aPaLEX>
2020-05-31 22:05:51 +0100

    test fix

--- jiotty-connector-slide/src/test/java/net/yudichev/jiotty/connector/slide/SlideApplianceModuleTest.java
+++ jiotty-connector-slide/src/test/java/net/yudichev/jiotty/connector/slide/SlideApplianceModuleTest.java
@@ -4,20 +4,30 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Key;
 import net.yudichev.jiotty.appliance.Appliance;
+import net.yudichev.jiotty.common.async.ExecutorFactory;
 import net.yudichev.jiotty.common.inject.ExposedKeyModule;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static net.yudichev.jiotty.common.inject.BindingSpec.literally;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+@ExtendWith(MockitoExtension.class)
 class SlideApplianceModuleTest {
+    @Mock
+    private ExecutorFactory executorFactory;
+
     @Test
     void test() {
         ExposedKeyModule<Appliance> applianceModule = SlideApplianceModule.builder()
                 .setSlideIdSpec(literally(1L))
                 .build();
-        Injector injector = Guice.createInjector(SlideServiceModule.builder()
+        Injector injector = Guice.createInjector(
+                binder -> binder.bind(ExecutorFactory.class).toInstance(executorFactory),
+                SlideServiceModule.builder()
                         .setEmail(literally("email"))
                         .setPassword(literally("password"))
                         .build(),
