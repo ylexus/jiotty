3e1da0fc0ff95cf78353240c542842d443273f83
ylexus <alexey@yudichev.net>
2023-12-02 10:58:24 +0000

    ignore unknown properties in sunrise-sunset responses and data

--- jiotty-connector-world/src/main/java/net/yudichev/jiotty/connector/world/BaseSunriseSunsetData.java
+++ jiotty-connector-world/src/main/java/net/yudichev/jiotty/connector/world/BaseSunriseSunsetData.java
@@ -1,5 +1,6 @@
 package net.yudichev.jiotty.connector.world;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import net.yudichev.jiotty.common.lang.PublicImmutablesStyle;
@@ -10,6 +11,7 @@ import java.time.Instant;
 @Value.Immutable
 @PublicImmutablesStyle
 @JsonDeserialize
+@JsonIgnoreProperties(ignoreUnknown = true)
 interface BaseSunriseSunsetData {
     Instant sunrise();
 
--- jiotty-connector-world/src/main/java/net/yudichev/jiotty/connector/world/BaseSunriseSunsetResponse.java
+++ jiotty-connector-world/src/main/java/net/yudichev/jiotty/connector/world/BaseSunriseSunsetResponse.java
@@ -1,5 +1,6 @@
 package net.yudichev.jiotty.connector.world;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import net.yudichev.jiotty.common.lang.PackagePrivateImmutablesStyle;
 import org.immutables.value.Value;
@@ -9,6 +10,7 @@ import java.util.Optional;
 @Value.Immutable
 @PackagePrivateImmutablesStyle
 @JsonDeserialize
+@JsonIgnoreProperties(ignoreUnknown = true)
 interface BaseSunriseSunsetResponse {
     String status();
 
