9584b66e503a2f10eca55222c792aae633283199
ylexus <alexey@yudichev.net>
2024-08-01 11:58:20 +0100

    even better pi4j shutdown

--- jiotty-connector-rpi/src/main/java/net/yudichev/jiotty/connector/rpigpio/Pi4jContextProvider.java
+++ jiotty-connector-rpi/src/main/java/net/yudichev/jiotty/connector/rpigpio/Pi4jContextProvider.java
@@ -11,13 +11,15 @@ import org.slf4j.LoggerFactory;
 import javax.inject.Provider;
 import java.util.concurrent.CountDownLatch;
 
-import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static net.yudichev.jiotty.common.lang.MoreThrowables.asUnchecked;
 
 final class Pi4jContextProvider extends BaseLifecycleComponent implements Provider<Context> {
     private static final Logger logger = LoggerFactory.getLogger(Pi4jContextProvider.class);
+    private static final int SHUTDOWN_TIMEOUT_SECONDS = 15;
 
-    private final CountDownLatch shutdownBlocker = new CountDownLatch(1);
+    private final CountDownLatch preShutdownLatch = new CountDownLatch(1);
+    private final CountDownLatch postShutdownLatch = new CountDownLatch(1);
     private volatile boolean pi4jShuttingDown;
     private Context gpio;
 
@@ -36,7 +38,7 @@ final class Pi4jContextProvider extends BaseLifecycleComponent implements Provid
                 pi4jShuttingDown = true;
                 logger.debug("Blocking pi4j shutdown until this component is closed");
                 asUnchecked(() -> {
-                    var stopTriggered = shutdownBlocker.await(1, MINUTES);
+                    var stopTriggered = preShutdownLatch.await(SHUTDOWN_TIMEOUT_SECONDS, SECONDS);
                     if (stopTriggered) {
                         logger.debug("Stopping - pi4j shutdown released");
                     } else {
@@ -47,6 +49,7 @@ final class Pi4jContextProvider extends BaseLifecycleComponent implements Provid
 
             @Override
             public void onShutdown(ShutdownEvent event) {
+                postShutdownLatch.countDown();
             }
         });
     }
@@ -54,10 +57,21 @@ final class Pi4jContextProvider extends BaseLifecycleComponent implements Provid
     @Override
     protected void doStop() {
         logger.debug("Releasing pi4j shutdown gate and shutting down pi4j");
-        shutdownBlocker.countDown();
-        // best effort prevent double shutdown call, will work if the pi4j shutdown hook fires first
-        if (!pi4jShuttingDown) {
-            gpio.shutdown();
+        preShutdownLatch.countDown();
+        // best effort prevent double shutdown call and awaiting successful shutdown, will work if the pi4j shutdown hook fires first
+        if (pi4jShuttingDown) {
+            // pi4's own shutdown hook is doing the job - wait for shutdown to finish
+            logger.debug("Waiting for pi4j to shut down");
+            asUnchecked(() -> {
+                var stopped = postShutdownLatch.await(SHUTDOWN_TIMEOUT_SECONDS, SECONDS);
+                if (stopped) {
+                    logger.debug("pi4j shut down");
+                } else {
+                    logger.warn("Timed out waiting for pi4 to shut down");
+                }
+            });
+        } else {
+            gpio.shutdown(); // does it synchronously
         }
     }
 }
