80ef3014e78afd3f67e5bf43715882e6cf511b8d
ylexus <alexey@yudichev.net>
2024-08-18 16:47:36 +0200

    fix TaskExecutor failure loss

--- jiotty-common/src/main/java/net/yudichev/jiotty/common/async/TaskExecutor.java
+++ jiotty-common/src/main/java/net/yudichev/jiotty/common/async/TaskExecutor.java
@@ -1,10 +1,17 @@
 package net.yudichev.jiotty.common.async;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
+import static net.yudichev.jiotty.common.lang.Runnables.guarded;
+
 public interface TaskExecutor extends Executor {
+    Logger logger = LoggerFactory.getLogger(TaskExecutor.class);
+
     <T> CompletableFuture<T> submit(Callable<T> task);
 
     default CompletableFuture<Void> submit(Runnable command) {
@@ -13,7 +20,7 @@ public interface TaskExecutor extends Executor {
 
     @Override
     default void execute(Runnable command) {
-        submit(toCallable(command));
+        submit(guarded(logger, "task", command));
     }
 
     static Callable<Void> toCallable(Runnable command) {
