a0acfc329784ff931e30f79e325a9285745d8ac4
ylexus <alexey@yudichev.net>
2024-10-16 22:48:56 +0100

    support for simple command responses

--- jiotty-common/src/main/java/net/yudichev/jiotty/common/rest/RestServers.java
+++ jiotty-common/src/main/java/net/yudichev/jiotty/common/rest/RestServers.java
@@ -1,5 +1,6 @@
 package net.yudichev.jiotty.common.rest;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.base.Throwables;
 import net.yudichev.jiotty.common.lang.Json;
 import net.yudichev.jiotty.common.lang.MoreThrowables;
@@ -7,10 +8,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spark.Response;
 
+import javax.annotation.Nullable;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 public final class RestServers {
     private static final Logger logger = LoggerFactory.getLogger(RestServers.class);
@@ -20,23 +22,26 @@ public final class RestServers {
 
     public static Object withErrorsHandledJson(String handlerName, Response response, CompletableFuture<?> handler) {
         response.header("Content-Type", "application/json");
-        return withErrorsHandled(handlerName, handler,
-                () -> Json.object()
-                        .put("success", "true")
-                        .toString(),
-                message -> Json.object()
-                        .put("success", "false")
-                        .put("errorText", message)
-                        .toString());
+        return withErrorsHandled(handlerName,
+                                 handler,
+                                 responseObj -> {
+                                     ObjectNode factory = Json.object().put("success", "true");
+                                     responseObj.ifPresent(theResponse -> factory.put("response", theResponse.toString()));
+                                     return factory.toString();
+                                 },
+                                 message -> Json.object()
+                                                .put("success", "false")
+                                                .put("errorText", message)
+                                                .toString());
     }
 
     private static Object withErrorsHandled(String handlerName,
                                             CompletableFuture<?> handler,
-                                            Supplier<Object> successSupplier,
+                                            Function<Optional<Object>, Object> successFactory,
                                             Function<String, Object> errorFactory) {
         try {
-            MoreThrowables.asUnchecked(() -> handler.get(3, TimeUnit.MINUTES));
-            return successSupplier.get();
+            @Nullable Object response = MoreThrowables.getAsUnchecked(() -> handler.get(3, TimeUnit.MINUTES));
+            return successFactory.apply(Optional.ofNullable(response));
         } catch (RuntimeException e) {
             logger.error("Failed to execute REST handler {}", handlerName, e);
             String message = Throwables.getRootCause(e).getMessage();
