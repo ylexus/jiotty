9ac027c9731d0cf79e3cb2e79de00516ffdaaf06
ylexus <alexey@yudichev.net>
2025-12-12 16:51:40 +0000

    docker to proper maven and jdk version + minor stuff

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM maven:3.8-jdk-11 AS build
+FROM maven:3.9.11-amazoncorretto-25 AS build
 COPY . /
 WORKDIR /
 RUN mvn -T1C clean verify
--- jiotty-connector-tesla/src/main/java/net/yudichev/jiotty/connector/tesla/fleet/TeslaChargingState.java
+++ jiotty-connector-tesla/src/main/java/net/yudichev/jiotty/connector/tesla/fleet/TeslaChargingState.java
@@ -2,6 +2,7 @@ package net.yudichev.jiotty.connector.tesla.fleet;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/// @implNote Values sneaked from [home assistant integration](https://github.com/home-assistant/core/blob/dev/homeassistant/components/tesla_fleet/sensor.py)
 public enum TeslaChargingState {
     @JsonProperty("Starting")
     STARTING,
